a6567f529d5b1a3d2d9a29becd151d301435e199
Dantrell Person <dantrellperson2@gmail.com>
2022-09-20 21:06:39 -0400

    Updating app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def scrape():
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mars.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
